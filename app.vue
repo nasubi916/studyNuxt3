db81fac7a98bfb275f1cabb027f9cc617a30c225
nasubi916 <r916nis@gmail.com>
2022-12-15 16:30:20 +0900

    fix

--- app.vue
+++ app.vue
@@ -1,16 +1,12 @@
 <template>
   <div>
-    count:{{count}}
+    count:{{ count }}
     <button @click="inc()">inc</button>
-    <button @click="dec()">dec</button>
   </div>
 </template>
 <script setup lang="ts">
 const count = ref(0);
 function inc() {
   count.value++;
-}
-function dec() {
-  count.value--;
 }
 </script>
